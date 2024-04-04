d1a8d292882f22fbbecd658e594af28df9fab2d1
Shawn Dai <79814208+JoinGame22@users.noreply.github.com>
2024-04-04 14:25:17 -0700

    buttonadd

--- src/app/modules/admin/example/example.component.ts
+++ src/app/modules/admin/example/example.component.ts
@@ -1,10 +1,14 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
     selector     : 'example',
     standalone   : true,
     templateUrl  : './example.component.html',
     encapsulation: ViewEncapsulation.None,
+    imports: [FormsModule, MatFormFieldModule, MatInputModule],
 })
 export class ExampleComponent
 {

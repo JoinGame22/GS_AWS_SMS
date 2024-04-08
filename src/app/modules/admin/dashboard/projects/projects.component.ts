3ef535c93dfd9192f98b27a325a8671d352b4868
Shawn Dai <79814208+JoinGame22@users.noreply.github.com>
2024-04-08 16:32:38 -0700

    cardtable

--- src/app/modules/admin/dashboard/projects/projects.component.ts
+++ src/app/modules/admin/dashboard/projects/projects.component.ts
@@ -4,16 +4,23 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import {MatIconModule} from '@angular/material/icon';
 import { MatButton, MatButtonModule } from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
     selector     : 'projects',
     standalone   : true,
     templateUrl  : './projects.component.html',
+    styleUrls: ['./project.component.css'],
     encapsulation: ViewEncapsulation.None,
-    imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+    imports: [FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatTabsModule, MatGridListModule, MatCardModule],
 })
 export class ProjectsComponent
 {
+
+
     /**
      * Constructor
      */
